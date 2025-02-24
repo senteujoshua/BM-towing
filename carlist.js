312c969b7b4376423246333ebfdcb8c38a999afa
WairiaGracee <gjblessed12@gmail.com>
2025-02-24 13:05:53 +0300

    done

--- carlist.js
+++ carlist.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(){
-    
+    const next = document.getElementById('gotonext');
+    const cont = document.getElementById('cont');
+
+    next.addEventListener('click', function(){
+        
+    })
 })
